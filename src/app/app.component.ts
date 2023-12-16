10f758443031782b89eb4710f4a4893427696ac3
ilampi <iliana.lampi@gmail.com>
2023-12-16 16:22:30 +0200

    Merge branch 'master' of https://github.com/pbrats/team3project into iliiana

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+<<<<<<< HEAD
 import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from "./header/header.component";
+=======
+import { HeaderComponent } from './header/header.component';
+>>>>>>> giouli
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [
-    CommonModule, 
-    RouterOutlet,
-    FooterComponent],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
+    imports: [
+        CommonModule,
+        RouterOutlet,
+        FooterComponent,
+        HeaderComponent
+    ]
 })
 export class AppComponent {
   title = 'team3project';

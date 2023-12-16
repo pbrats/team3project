3f343228d5467cfc7d6f3b6d8a5b7c7497d40038
ilampi <iliana.lampi@gmail.com>
2023-12-16 15:41:16 +0200

    created footer component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from './footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [
+    CommonModule, 
+    RouterOutlet,
+    FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })

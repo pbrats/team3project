dfda0851b8fd8fcba8d63dd0ecfd07f802ecb34b
pbrats <p.bratsioti@gmail.com>
2024-01-04 19:45:26 +0200

    landing page header creation

--- src/app/component/landing-header/landing-header.component.ts
+++ src/app/component/landing-header/landing-header.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-landing-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterLinkActive,CommonModule],
   templateUrl: './landing-header.component.html',
   styleUrl: './landing-header.component.css'
 })

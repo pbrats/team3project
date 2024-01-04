f14b2401b00036279a5064c07ca79778146134f9
pbrats <p.bratsioti@gmail.com>
2024-01-04 19:35:13 +0200

    header creation

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [RouterLink,RouterLinkActive,CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })

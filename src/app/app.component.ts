212f3f1eb763a547ce987c629f82fdb8c8cce62f
pbrats <p.bratsioti@gmail.com>
2024-01-04 20:17:11 +0200

    app edit app routes paths creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { LandingHeaderComponent } from './component/landing-header/landing-header.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css'],
-    imports: [
-     
-    ]
+    imports: [CommonModule,RouterOutlet,HeaderComponent,FooterComponent,LandingHeaderComponent]
 })
 export class AppComponent {
-  title = 'team3project';
+  title = 'Delivery';
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { NotFoundComponent } from './component/not-found/not-found.component';
+import { CategoriesComponent } from './component/categories/categories.component';
+import { AllStoresComponent } from './component/all-stores/all-stores.component';
+import { FamousStoresGeneralComponent } from './component/famous-stores-general/famous-stores-general.component';
+import { MainComponent } from './component/main/main.component';
+import { LandingPageComponent } from './component/landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: "", redirectTo: "welcome", pathMatch:"full"},
+    {path: "welcome", component: LandingPageComponent},
+    {path: "discovery", component: MainComponent},
+    {path: "famous-stores", component: FamousStoresGeneralComponent},
+    {path: "stores", component: AllStoresComponent},
+    {path: "categories", component: CategoriesComponent},
+    {path: "**", component: NotFoundComponent}
+];

c0c3eb811601f2787897d1d3f026d19b3ffdadc4
pbrats <p.bratsioti@gmail.com>
2024-01-18 19:30:45 +0200

    support page edited contact-form added

--- src/app/component/support/support.component.ts
+++ src/app/component/support/support.component.ts
@@ -2,18 +2,21 @@ import { Component, inject } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { PublisherService } from '../../service/publisher.service';
 import { NavigationEnd, Router } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-support',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './support.component.html',
   styleUrl: './support.component.css'
 })
 export class SupportComponent {
   publisherService =inject(PublisherService);
   isSupportPage=true;
-  constructor(private titleService: Title,private router: Router) {
+  FormData!: FormGroup;
+  
+  constructor(private titleService: Title,private router: Router,private builder: FormBuilder) {
     titleService.setTitle("Support");
     this.isSupportPage=true;
     this.publisherService.publishData(this.isSupportPage);
@@ -28,5 +31,15 @@ export class SupportComponent {
           }
         }
       });
+  }
+  ngOnInit() {
+    this.FormData = this.builder.group({
+    Fullname: new FormControl('', [Validators.required]),
+    Email: new FormControl('', [Validators.required]),
+    Comment: new FormControl('', [Validators.required])
+    })
+  }
+  onSubmit(FormData: any) {
+    console.log(FormData)
   }
 }

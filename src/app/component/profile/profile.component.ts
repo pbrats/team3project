e191780c33e667fe8d2e6f9e214d93f385978700
ilampi <iliana.lampi@gmail.com>
2024-01-25 21:10:00 +0200

    translated user info to english - added change address button in profile page

--- src/app/component/profile/profile.component.ts
+++ src/app/component/profile/profile.component.ts
@@ -1,11 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule, FormsModule],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.css'
+  styleUrl: './profile.component.css',
 })
 export class ProfileComponent {
   phone!: string;
@@ -14,17 +22,50 @@ export class ProfileComponent {
   last_name!: string;
   address!: string;
 
+  addressForm!: FormGroup;
 
-  ngOnInit(){ //get user data from session storage
-    this.phone = JSON.stringify(sessionStorage.getItem("phone")).replace(/"/g, "");
-    this.email = JSON.stringify(sessionStorage.getItem("email")).replace(/"/g, "");
-    this.first_name = JSON.stringify(sessionStorage.getItem("first_name")).replace(/"/g, "");
-    this.last_name = JSON.stringify(sessionStorage.getItem("last_name")).replace(/"/g, "");
-    this.address = JSON.stringify(sessionStorage.getItem("address")).replace(/"/g, "");
+  newAddressBlock: boolean = false;
+
+  ngOnInit() {
+    //get user data from session storage
+    this.phone = JSON.stringify(sessionStorage.getItem('phone')).replace(
+      /"/g,
+      ''
+    );
+    this.email = JSON.stringify(sessionStorage.getItem('email')).replace(
+      /"/g,
+      ''
+    );
+    this.first_name = JSON.stringify(
+      sessionStorage.getItem('first_name')
+    ).replace(/"/g, '');
+    this.last_name = JSON.stringify(
+      sessionStorage.getItem('last_name')
+    ).replace(/"/g, '');
+    this.address = JSON.stringify(sessionStorage.getItem('address')).replace(
+      /"/g,
+      ''
+    );
+
+    this.addressForm = new FormGroup({
+      address: new FormControl("", Validators.required),
+    });
   }
 
-  updateAddress(newAddress: string){
-    sessionStorage.setItem("address", newAddress);
-    this.address = JSON.stringify(sessionStorage.getItem("address")).replace(/"/g, "");
+  onSubmit() {
+    if (this.addressForm.valid) {
+      sessionStorage.setItem("address", this.addressForm.value.address);
+      this.address = JSON.stringify(sessionStorage.getItem('address')).replace(
+        /"/g,
+        ''
+      );
+    }
+    else{
+      this.addressForm.markAllAsTouched();
+    }
+  }
+
+  showNewAddress() {
+    this.newAddressBlock = true;
   }
 }
